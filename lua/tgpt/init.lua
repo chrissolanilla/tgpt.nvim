6bd4dc215ece80b9e872053d7a2b34d2ce5436c8
Christopher Solanilla <chrissolanilla@gmail.com>
2025-04-24 12:10:06 -0400

    makes it easier to exit

--- lua/tgpt/init.lua
+++ lua/tgpt/init.lua
@@ -15,14 +15,30 @@ local createBuffer = function ()
     })
 end
 
-local InteractiveChat = function ()
-    createBuffer()
-    vim.api.nvim_command("startinsert")
-    vim.fn.termopen("tgpt -i", {on_exit = function ()
-        local win_id = vim.api.nvim_get_current_win()
-        vim.api.nvim_win_close(win_id, true)
-    end})
+-- local InteractiveChat = function ()
+--     createBuffer()
+--     vim.api.nvim_command("startinsert")
+--     vim.fn.termopen("tgpt -i", {on_exit = function ()
+--         local win_id = vim.api.nvim_get_current_win()
+--         vim.api.nvim_win_close(win_id, true)
+--     end})
+-- end
+local InteractiveChat = function()
+    -- Create a new split window for the chat
+    vim.cmd("botright split")
+    vim.cmd("resize 15")  -- optional: control chat window height
+    vim.cmd("terminal tgpt -i")
+
+    -- Keymap: Ctrl-c to go to normal mode
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-c>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+    -- Keymap: Ctrl-d to close the terminal
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-d>', [[<C-\><C-n>:bd!<CR>]], { noremap = true, silent = true })
+
+    -- Enter insert mode so you can start chatting right away
+    vim.cmd("startinsert")
 end
+
 
 local RateMyCode = function ()
    local file = vim.api.nvim_buf_get_name(0)
@@ -47,7 +63,7 @@ function M.setup()
             nargs = 0,
         })
         vim.api.nvim_create_user_command("RateMyCode",
-           RateMyCode 
+           RateMyCode
         , {
             nargs = 0,
         })
